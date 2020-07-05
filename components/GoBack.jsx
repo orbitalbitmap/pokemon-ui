c7cc5ed96b50e24b09394cfe61e618cb1cb87978
orbitalbitmap <orbitalbitmap@gmail.com>
2020-07-04 21:54:48 -0400

    Fix the missing \'<<Go Back\' when the /pages/PokemonDetailsPage.jsx is rendered.

--- components/GoBack.jsx
+++ components/GoBack.jsx
@@ -5,8 +5,12 @@ import styled from 'styled-components'
 const Link = styled(NavLink)`
   color: white;
   font-size: 20px;
+  position: fixed;
   text-align: left;
   text-decoration: none;
+  top: 100px;
+  left: 30px;
+  z-index: 10;
 
   &:hover {
     color: magenta;
